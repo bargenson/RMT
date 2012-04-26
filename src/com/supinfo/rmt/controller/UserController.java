0aba3be848db500c77a12d68b49c9fde478ad7fa
bargenson <bargenson@gmail.com>
2012-04-26 18:19:18 +0200

    Login Form

---
+++ src/com/supinfo/rmt/controller/UserController.java
@@ -0,0 +1,69 @@
+package com.supinfo.rmt.controller;
+
+import com.supinfo.rmt.entity.User;
+import com.supinfo.rmt.exception.AuthenticationException;
+import com.supinfo.rmt.service.UserService;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.NoResultException;
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bargenson
+ * Date: 4/26/12
+ * Time: 4:01 PM
+ */
+@ManagedBean
+@SessionScoped
+public class UserController implements Serializable {
+
+    @EJB
+    private UserService userService;
+
+    private String username;
+    private String password;
+
+    private User user;
+
+    public String login() {
+        try {
+            user = userService.authenticate(username, password);
+            return "authenticated";
+        } catch (AuthenticationException e) {
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Bad credentials, please try again.", null)
+            );
+            return null;
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
